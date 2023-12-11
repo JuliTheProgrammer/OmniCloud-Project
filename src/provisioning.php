5982f3eabc80b76bc001566182c3cf273a4f654b
JuliTheProgrammer <juli.leipert@gmail.com>
2023-12-11 15:29:02 +0100

    add logic.php and make the logic to provision servers

--- src/provisioning.php
+++ src/provisioning.php
@@ -29,65 +29,10 @@ $ssdOptions = array(
 );
 
 
-$CPU = "";
-$RAM = "";
-$SSD = "";
-
-if (isset($_POST['submit'])) {
-  $CPU = $_POST['CPU'];
-  $RAM = $_POST['RAM'];
-  $SSD = $_POST['SSD'];
-  echo $CPU;
-  echo $RAM;
-  echo $SSD;
-}
-
-
-
-
-// Convert CPU, RAM, and SSD to integers
-$CPU = (int) $CPU;
-$RAM = (int) $RAM;
-$SSD = (int) $SSD;
-
 //Can also loop up in array the value of the CPU, RAM, and SSD
-$totalPrice = $CPU * 5 + $RAM * 5 + $SSD * 10;
+//$totalPrice = $CPU * 5 + $RAM * 5 + $SSD * 10;
 
 
-//Programm the logic for the provisioning
-
-//Get data from the php file and decode it
-$serverData = file_get_contents("data.json");
-$serverData = json_decode(json_encode($serverData), true);
-
-//Make classes for each server
-
-class Server {
-  public $type;
-  public $id;
-  public $price;
-  public $cpu;
-  public $ram;
-  public $ssd;
-}
-
-$serverArray = array();
-
-//Loop through the data and assign it to a global variable
-foreach ($serverData as $key => $value) {
-    //Assign it to a object
-    $server = new Server();
-    $server->type = $value['type'];
-    $server->id = $value['id'];
-    $server->price = $value['price'];
-    $server->cpu = $value['cpu'];
-    $server->ram = $value['ram'];
-    $server->ssd = $value['ssd'];
-    //Push the object to the array
-    array_push($serverArray, $server);
-};
-
-echo $serverData->servers[0]->type;
 ?>
 
 <!DOCTYPE html>
@@ -104,7 +49,7 @@ echo $serverData->servers[0]->type;
         <h2 class="provisioning-title">Our Servers, Your Customization</h2>
         <section class="provisioning-section-container">
           <div class="provisioning-section-container-padding">
-            <form method="post" action="provisioning.php" class="provisioning-section">
+            <form method="POST" action="dashboared.php" class="provisioning-section">
               <h3>Choose CPU Cores</h3>
               <!--Loop through the array and create a checkbox for each option-->
               <?php foreach ($cpuOptions as $key => $value) : ?>
@@ -114,7 +59,7 @@ echo $serverData->servers[0]->type;
                   <p><?php echo $value; ?> CHF</p>
                 </div>
                 <?php endforeach; ?> 
-            </form>
+           
             <div class="provisioning-section">
             <h3>Choose RAM Size</h3>
                 <!--Loop through the array and create a checkbox for each option-->
@@ -137,17 +82,18 @@ echo $serverData->servers[0]->type;
                 </div>
                 <?php endforeach; ?> 
                 </div>
-
+                
           </div>
           
         <section>
       <section class="provisioning-bottom-container">
       <div class="provisioning-total-price-container">
-          <p><?php echo $totalPrice; ?> CHF</p>
+          <!-- <p><?php echo $totalPrice; ?> CHF</p> -->
       </div>
       <div class="confirm-button-container">
         <input type="submit" name="submit"></input>
       </div>
+      </form>
     </section>
        
     </main>
